f64312d554053784a0dc9fdc757aeb49008453e4
sandeep-dev8639 <reniguntlasandeep123@gmail.com>
2026-01-31 00:05:49 -0800

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class Max {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b){
+            System.out.println("Max:"+a);
+        }
+        else{
+            System.out.println("Max:"+b);
+        }
+    }
+}
