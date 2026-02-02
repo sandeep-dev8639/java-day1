c99299d432e16a5c41e6b3314e1f854f24ceca0f
sandeep-dev8639 <reniguntlasandeep123@gmail.com>
2026-02-02 07:53:02 -0800

    Add files via upload

--- Loops.java
+++ Loops.java
@@ -4,21 +4,6 @@ public class Loops {
         for(int i=0;i<=10;i++){
             System.out.println(2+"*"+i+"="+i*2);
             System.out.println(2);
-
         }
-       
-
-       /* int i=0;
-        while (i<10) {
-            System.out.println(i);
-        i++;
-        }*/
-
-       /*  int i = 1;
-        do{
-            System.out.println(i);
-            i++;
-        }while(i<11);*/
     }
-    
 }
