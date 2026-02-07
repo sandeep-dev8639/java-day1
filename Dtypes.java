53b7a3ab58f5cb2332321e75090d94a0ee038df0
R Sandeep kumar <reniguntlasandeep123@gmail.com>
2026-02-07 11:02:00 +0530

    Add files via upload

---
+++ Dtypes.java
@@ -0,0 +1,16 @@
+public class Dtypes {
+    public static void main(String[] args) {
+        byte b=120;
+        short s=159;
+        int i=12000000;
+        float f=10.99999f;
+        double d=10.33223d;
+        char ch = 'a';
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+    }
+}
