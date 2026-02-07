0f0f9c8dc070138aa395e897645341d129641842
R Sandeep kumar <reniguntlasandeep123@gmail.com>
2026-02-07 11:01:03 +0530

    Add files via upload

---
+++ Token.java
@@ -0,0 +1,22 @@
+public class Token {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 071;
+        int c = 0x2A;
+        int d = 0b1001;
+        System.out.println("decimal="+a);
+        System.out.println("octal="+b);
+        System.out.println("hexa decimal="+c);
+        System.out.println("binary="+d);
+        float f=3.14f;
+        double x = 3.4475778d;
+        System.out.println("float="+f);
+        System.out.println("Double="+x);
+        char z = 97;
+        System.out.println("character="+z);
+        boolean boolt = true;
+        boolean boolf= false;
+        System.out.println("boolean for true="+boolt);
+        System.out.println("boolean for false="+boolf); 
+    }
+}
